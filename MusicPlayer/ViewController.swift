82e9640c4b47384ed9e04e3cdd10cd0003cf7d39
mhdaffan <muhammad.affan@thelionparcel.com>
2022-03-09 17:48:54 +0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Muhammad Affan on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
